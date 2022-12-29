8640f3e5aad11e5ed6372d26d48840ba1e95c4d3
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2022-12-29 01:06:46 -0300

    JWT - Token

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,34 +1,52 @@
 import { compare } from "bcryptjs";
 import prismaClient from "../../prisma";
+import { sign } from "jsonwebtoken";
 
 
-interface AuthRequest{
+interface AuthRequest {
     email: string;
     password: string;
 };
 
-class AuthUserService{
+class AuthUserService {
 
-    async execute({email, password}: AuthRequest){
-        
+    async execute({ email, password }: AuthRequest) {
+
         // Email not existent
         const user = await prismaClient.user.findFirst({
-            where:{
+            where: {
                 email: email,
             }
         })
 
-        if (!user) {throw new Error("Incorrect user or password")}
+        if (!user) { throw new Error("Incorrect user or password") }
 
         // Check if password is correct
 
         const correctPassword = await compare(password, user.password);
-        if (!correctPassword) {throw new Error("Incorrect user or password")};
+        if (!correctPassword) { throw new Error("Incorrect user or password") };
 
+        // Generate JWT token and return id, email and name
+
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email,
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
         
-
-        return {Auth: true};
-
     }
 
 }
