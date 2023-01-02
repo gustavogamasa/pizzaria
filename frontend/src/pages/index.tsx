acc453c88e3dee318406bcc1d2e94153cff6ec4f
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2023-01-01 21:19:03 -0300

    Button - Component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,6 +3,7 @@ import styles from '../styles/home.module.scss'
 import logoImg from '../../public/pizza_svg_logo.svg'
 import Image from "next/image";
 import { Input } from "../components/ui/Input";
+import { Button } from "../components/ui/Button";
 
 
 
@@ -31,6 +32,13 @@ export default function Home() {
               placeholder="Senha"
               type="password"
             />
+            
+            <Button 
+            type="submit"
+            loading={true}
+            >
+              Acessar
+            </Button>
           </form>
 
 
